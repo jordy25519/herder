610612ca11a54420bbab358ba235a8f90155bac2
thelastairbender <beauchjord@gmail.com>
2015-08-04 23:57:38 +1200

    d3 charting improvements.

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -2,23 +2,17 @@ class TestController < ApplicationController
   skip_before_filter :verify_authenticity_token, only: [:index]
 	
 	def index
+
+		# Count the number of files in each test.
+		@sizes = Array.new
+		Dir.glob('./tests/*').each do |d|
+			@sizes << Dir.glob(d + "/*").count
+		end
+		
 		@tests =  Dir.glob('./tests/*')
 		@tests.map! { |e| e.split("/").last + "/" }
-		if request.post?
-			@test = params[:test]
-			@size = Dir.glob(@test + "*").count
-			redirect_to 'index'
-		else
-			@test = Dir.glob('./tests/*').first + "/"
-			@size = Dir.glob(@test + "*").count
-		end
-	end
-
-	def io
-	end
-
-	def cpu
-		@path = "/home/jordanbeauchamp/projects/herder/tests/aviarcs 10 2015-07-23 10:55/"
+		
+		@test = Dir.glob('./tests/*').first + "/"
 	end
 
 end
