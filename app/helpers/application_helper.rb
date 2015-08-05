883b668fa25073f1d78b5c867239953a175a1cd7
thelastairbender <beauchjord@gmail.com>
2015-08-06 10:41:14 +1200

    Updated rails app to view new data fields.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,14 @@
 module ApplicationHelper
 # Return list of filter options for CSV.
 	def filter_options
-		["Memory (bytes)",
-		"CPU usage (USER_HZ)",
-		"CPU time (nanoseconds)",
-		"IO paged (bytes)",
-		"IO unpaged (bytes)"] 
+		[
+			"Memory (bytes)",
+			"Memory instant (bytes)",
+			"CPU usage (USER_HZ)",
+			"CPU time (nanoseconds)",
+			"CPU instant (nanoseconds)",
+			"IO paged (bytes)",
+			"IO paged instant (bytes)",
+			"IO unpaged (bytes)"] 
 	end
 end
